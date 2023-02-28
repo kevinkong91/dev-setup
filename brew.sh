16e7b55a718e3052fabd39180a4804dfdab8c44b
Kevin Kong <kevinkong@kevins-mbp.lan>
2023-02-27 23:34:09 -0500

    feat: htop, tig, tmux

--- brew.sh
+++ brew.sh
@@ -42,8 +42,8 @@ sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
 # Change to the new shell, prompts for password
 chsh -s /usr/local/bin/bash
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
+# Install `wget` 
+brew install wget
 
 # ZSH
 brew install zsh
@@ -118,6 +118,7 @@ brew install git-lfs
 brew install git-flow
 brew install git-extras
 brew install hub
+brew install htop
 brew install imagemagick --with-webp
 brew install lua
 brew install lynx
@@ -128,7 +129,9 @@ brew install rename
 brew install rhino
 brew install speedtest_cli
 brew install ssh-copy-id
+brew install tig
 brew install tree
+brew install tmux
 brew install webkit2png
 brew install zopfli
 brew install pkg-config libffi
